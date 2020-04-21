ad20706eba62ce691a6614e0301994ba186d54ef
HaciIsma <haciismayilov5@outlook.com>
2020-04-21 07:33:53 +0400

    4 commit

---
+++ ViewModel/AddViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using WpfHomework1.Command;
+using WpfHomework1.Model;
+
+namespace WpfHomework1.ViewModel
+{
+    class AddViewModel
+    {
+        public Car CarExample { get; set; }
+        public ICommand SaveCommand { get; set; }
+        public AddViewModel()
+        {
+            SaveCommand = new RelayCommand(SaveCommandExecute);
+        }
+        public void SaveCommandExecute(object param)
+        {
+            MessageBox.Show("Saved");
+        }
+    }
+}
