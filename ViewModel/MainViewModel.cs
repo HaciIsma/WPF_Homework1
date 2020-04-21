54a4c1780dffe1a655fb46bf7d1abd20b00fc8bd
HaciIsma <haciismayilov5@outlook.com>
2020-04-21 08:35:40 +0400

    6 commit

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows;
 using System.Windows.Input;
 using WpfHomework1.Command;
 using WpfHomework1.Model;
@@ -12,7 +10,10 @@ namespace WpfHomework1.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Car> Cars { get; set; } = new ObservableCollection<Car>();
+        public ObservableCollection<Car> Cars { get; set; } = new ObservableCollection<Car>
+        {
+            new Car{Model="BMW",Vendor="X5",Photo="X5.png"}
+        };
 
         public ICommand AddCommand { get; set; }
 
@@ -26,6 +27,7 @@ namespace WpfHomework1.ViewModel
             AddCommandView addView = new AddCommandView(this);
             addView.Show();
         }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
