138257b9b843e106d5830ed0d24ad85c231e5bd6
HaciIsma <haciismayilov5@outlook.com>
2020-04-23 04:53:24 +0400

    11 commit

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using WpfHomework1.Command;
@@ -12,28 +15,67 @@ namespace WpfHomework1.ViewModel
     {
         public ObservableCollection<Car> Cars { get; set; } = new ObservableCollection<Car>
         {
-            new Car{Model="BMW",Vendor="X5",Photo="X5.png"}
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
+            new Car{Model="BMW",Vendor="X5",Photo=@"Resource/X5.png"},
         };
 
+        private string saveButtonVisible = "Hidden";
+        public string SaveButtonVisible
+        {
+            get
+            {
+                return saveButtonVisible;
+            }
+            set
+            {
+                saveButtonVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
         public int SelectedCarIndex { get; set; }
-        public ICommand AddCommand  { get; set; }
+        public ICommand AddCommand { get; set; }
         public ICommand EditCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
 
         public MainViewModel()
         {
-            AddCommand  = new RelayCommand(AddCommandExecute);
+            AddCommand = new RelayCommand(AddCommandExecute);
             EditCommand = new RelayCommand(EditCommandExecute);
+            SaveCommand = new RelayCommand(SaveCommandExecute);
+            SaveButtonVisible = Cars.Count >= 10 ? "Visible" : "Hidden";
         }
 
         public void AddCommandExecute(object param)
         {
             AddCommandView addView = new AddCommandView(this);
-            addView.Show();
+            addView.ShowDialog();
+
+            if (Cars.Count >= 10)
+            {
+                SaveButtonVisible = "Visible";
+            }
         }
         public void EditCommandExecute(object param)
         {
             EditView editView = new EditView(this);
-            editView.Show();
+            editView.ShowDialog();
+        }
+
+        public void SaveCommandExecute(object param)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Json file (*.json)|*.json";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, JsonConvert.SerializeObject(Cars));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
--- obj/Debug/WpfHomework1_Content.g.i.cs
+++ obj/Debug/WpfHomework1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("x5.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pencil.png")]
 
 
